92475b2ab402dbf291dfe61ad0b4a64938819cb5
JoshuaSimmons0425 <jjsimmons0425@gmail.com>
2026-09-12 18:32:04 +1200

    Fixed bug to which uses correct data type to store positive weights

---
+++ src/modelling/multilabel/bert/ml_bert_model.py
@@ -0,0 +1,176 @@
+import os
+import json
+import time
+import torch
+import numpy as np
+import torch.nn as nn
+from transformers import AutoModel
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+from sklearn import metrics
+
+class MultiLabelBERTClassifier(nn.Module):
+    def __init__(self, n_labels, all_labels, train_loader, val_loader, optimizer, epochs, learning_rate, pretrained_model_name = "answerdotai/ModernBERT-base"):
+        super(MultiLabelBERTClassifier, self).__init__()
+        self.bert = AutoModel.from_pretrained(pretrained_model_name)
+        self.drop = nn.Dropout(p=0.3)
+        self.out = nn.Linear(self.bert.config.hidden_size, n_labels)
+        self.all_labels = all_labels
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.optimizer = optimizer
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.criterion = None
+
+        self.training_losses = []
+        self.validation_losses = []
+        self.eval_probs = []
+        self.eval_true = []
+
+    def forward(self, input_ids, attention_mask):
+
+        outputs = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        last_hidden = outputs.last_hidden_state
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(last_hidden.size()).float()
+        sum_embeddings = torch.sum(last_hidden * input_mask_expanded, 1)
+        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        pooled_output = sum_embeddings / sum_mask
+
+        output = self.drop(pooled_output)
+        return self.out(output)
+
+    def calculate_pos_weights(self, device):
+        # Calculate positive weights for each label based on the training dataset for cost-sensitive learning
+        raw_matrix = self.train_loader.dataset.labels 
+        labels_matrix = torch.tensor(raw_matrix, dtype=torch.float32)
+        num_samples = labels_matrix.shape[0]
+        pos_counts = torch.sum(labels_matrix, dim=0)
+        neg_counts = num_samples - pos_counts
+
+        pos_counts = torch.clamp(pos_counts, min=1)
+        neg_counts = torch.clamp(neg_counts, min=1)
+
+        pos_weights = neg_counts / pos_counts
+
+        self.criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weights.to(device))
+
+    def train_model(self, device):
+
+        self.to(device)
+
+        if self.optimizer == "adamw":
+            optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
+        else:
+            optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+        assert self.criterion is not None, "Criterion (loss function) must be set before training. Call calculate_pos_weights() first."
+
+        criterion = self.criterion
+        epochs = self.epochs
+
+        dev_type = 'cuda' if 'cuda' in str(device) else 'cpu'
+
+        amp_dtype = torch.float16 if dev_type == 'cuda' else torch.bfloat16
+
+        scaler = torch.amp.GradScaler(enabled=(dev_type == 'cuda'))
+        accumulation_steps = 4
+
+        if self.training_losses is not None and self.validation_losses is not None:
+            self.training_losses = [] # Reset loss lists at the start of training
+            self.validation_losses = []
+
+        print(f"Starting training on {dev_type.upper()} for {epochs} epochs with optimizer {self.optimizer} and learning rate {self.learning_rate}" + "\n")
+
+        time_per_epoch = []
+
+        for epoch in range(epochs):
+            self.train()
+            start_time = time.time()
+            total_loss = 0
+            optimizer.zero_grad()
+
+            for batch_idx, batch in enumerate(self.train_loader):
+                input_ids = batch['input_ids'].to(device)
+                attention_mask = batch['attention_mask'].to(device)
+                labels = batch['labels'].to(device)
+
+                with torch.amp.autocast(device_type=dev_type, dtype=amp_dtype):
+                    outputs = self(input_ids=input_ids, attention_mask=attention_mask)
+                    loss = criterion(outputs, labels)
+                    loss = loss / accumulation_steps  # Normalize the loss for gradient accumulation
+
+                if scaler is not None:
+                    scaler.scale(loss).backward()
+                    if (batch_idx + 1) % accumulation_steps == 0 or (batch_idx + 1) == len(self.train_loader):
+                        scaler.step(optimizer)
+                        scaler.update()
+                        optimizer.zero_grad()
+
+                else:
+                    loss.backward()
+                    if (batch_idx + 1) % accumulation_steps == 0 or (batch_idx + 1) == len(self.train_loader):
+                        optimizer.step()
+                        optimizer.zero_grad()
+
+                total_loss += loss.item() * accumulation_steps  # Scale the loss back up for reporting
+
+            self.training_losses.append(total_loss / len(self.train_loader))
+            epoch_time = (time.time() - start_time) / 60 # time in minutes
+            time_per_epoch.append(epoch_time)
+
+            self.eval()
+            validation_loss = 0
+            with torch.no_grad():
+                for val_batch in self.val_loader:
+                    val_input_ids = val_batch['input_ids'].to(device)
+                    val_attention_mask = val_batch['attention_mask'].to(device)
+                    val_labels = val_batch['labels'].to(device)
+
+                    val_outputs = self(val_input_ids, val_attention_mask)
+                    val_loss = criterion(val_outputs, val_labels)
+                    validation_loss += val_loss.item()
+
+            self.validation_losses.append(validation_loss / len(self.val_loader))
+
+            print(f"Epoch [{epoch + 1}/{epochs}], Training Loss: {self.training_losses[-1]:.4f}, Validation Loss: {self.validation_losses[-1]:.4f}, Time: {epoch_time:.2f} minutes")
+            print(f"Average Time per Epoch: {sum(time_per_epoch) / len(time_per_epoch):.2f} minutes" + "\n")
+
+        print(f"Final Training Loss: {self.training_losses[-1]:.4f}, Final Validation Loss: {self.validation_losses[-1]:.4f}" + "\n")
+
+    def evaluate_model(self, device):
+        self.eval()
+        self.to(device)
+
+        self.eval_probs = []
+        self.eval_true = []
+
+        print("Running validation evaluation with 0.5 decision threshold...")
+        for batch in self.val_loader:
+            with torch.no_grad():
+                output = self(batch['input_ids'].to(device), batch['attention_mask'].to(device))
+                probs = torch.sigmoid(output)
+
+            self.eval_probs.extend(probs.cpu().numpy())
+            self.eval_true.extend(batch['labels'].cpu().numpy())
+
+        self.eval_probs = np.vstack(self.eval_probs)
+        self.eval_true = np.vstack(self.eval_true)
+
+        preds = (self.eval_probs >= 0.5).astype(int)
+        self.accuracy = metrics.accuracy_score(self.eval_true, preds)
+        class_names = [f"Label {i}" for i in range(len(self.all_labels))]
+        self.classification_report = classification_report(self.eval_true, preds, target_names=class_names, zero_division=0)
+
+        print(f"Validation Accuracy: {self.accuracy:.4f}")
+        print("Classification Report:")
+        print(self.classification_report)
+
+    def run_pipeline(self, device):
+        self.to(device)
+        self.calculate_pos_weights(device)
+        self.train_model(device)
+        self.evaluate_model(device)
