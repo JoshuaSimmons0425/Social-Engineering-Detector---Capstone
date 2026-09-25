ce5a5396c3c4faaa074f99749008e727ad41a585
JoshuaSimmons0425 <jjsimmons0425@gmail.com>
2026-09-26 01:12:27 +1200

    Risk assessment with bert evidence confirmed to work as intended

--- scripts/generate_risk_assessment.py
+++ scripts/generate_risk_assessment.py
@@ -1,3 +1,4 @@
+import sys
 from src.explaination.ai_risk_assessor import AIRiskAssessor
 
 def main():
@@ -8,9 +9,20 @@ def main():
     user_prompt_path = 'prompts/explainer/explainer_prompt.md'
     system_prompt_path = 'prompts/explainer/system_prompt.md'
 
-    model_name = "qwen2.5-1.5b-instruct"
+    bert_evidence_path = "xAI_outputs/bert/explanations.txt"
 
-    input_text = "Notice, your account requires verification. To restore access, reset your password and enter your username, 2FA verification code, and recovery key.\n\nAccount Services,\nMorgan Martinez"
+    with open(bert_evidence_path, 'r') as f:
+        bert_evidence = f.read()
+
+    model_name = "llama-3.2-3b-instruct"
+
+    input_text = """Buck up, your troubles caused by small dimension will soon be over!
+Become a lover no woman will be able to resist!
+http://whitedone.com/
+
+
+come. Even as Nazi tanks were rolling down the streets, the dreamersphilosopher or a journalist. He was still not sure.I do the same."""
+
 
     assessor = AIRiskAssessor(
         model_id=model_name,
@@ -19,11 +31,14 @@ def main():
         base_url=base_url,
         user_prompt_path=user_prompt_path,
         system_prompt_path=system_prompt_path,
-        temperature=0.01
+        temperature=0.4,
+        evidence=bert_evidence
     )
 
     response = assessor.call_chat_model()
     print(response)
+
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
